3bfca25c96a05654acaabac14c9b943e23012322
Chris Cattano <christiancattano@gmail.com>
2022-05-19 19:33:30 -0500

    Finalized contents of the About page

--- Source/WebService/ClientApp/src/app/components/about/about.component.ts
+++ Source/WebService/ClientApp/src/app/components/about/about.component.ts
@@ -1,8 +1,7 @@
 import {Component} from '@angular/core';
 
 @Component({
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  templateUrl: './about.component.html'
 })
 export class AboutComponent {
 }
